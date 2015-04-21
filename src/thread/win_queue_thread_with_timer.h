6696863f2426d4c41bd801b8c780e0491eda82ab
xuhailong <hailong.hsu@gmail.com>
2015-04-21 14:28:06 +0800

    adds win ui thread

--- src/thread/win_queue_thread_with_timer.h
+++ src/thread/win_queue_thread_with_timer.h
@@ -1,4 +1,13 @@
 
+/*
+	author: xuhailong
+	date: 2011-4-18
+	msn: hailongxu@msn.com
+	Hi: hailonghsu@baidu.com
+
+	this is a free and open code
+	you can use it freely, but you should include the author declaration
+ */
 
 
 
