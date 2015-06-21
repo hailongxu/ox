930d129d7ad7c46b6c461fd0ce2921a3861825da
xuhailong <hailong.hsu@gmail.com>
2015-06-21 18:58:39 +0800

    add vector

---
+++ src/fit/allocator_default.h
@@ -0,0 +1,133 @@
+
+#include <new.h>
+#include "../ox/nsab.h"
+
+
+
+
+___namespace2_begin(ox,fit)
+
+
+template <typename data_tn>
+struct cppmalloc_tt
+{
+	char* allocate(size_t size)
+	{
+		return new (std::nothrow) char [size];
+	}
+	void deallocate(void* p)
+	{
+		delete [] p;
+	}
+	data_tn _m_data;
+};
+template <>
+struct cppmalloc_tt<void>
+{
+	char* allocate(size_t size)
+	{
+		return new (std::nothrow) char [size];
+	}
+	void deallocate(void* p)
+	{
+		delete [] p;
+	}
+};
+typedef cppmalloc_tt<void> cppmalloc;
+
+struct nullmalloc
+{
+	char* allocate(size_t size)
+	{
+		return 0;
+	}
+	void deallocate(void* p)
+	{
+	}
+};
+template <typename mallocator,size_t size_added_tc>
+struct cppmalloc_more : mallocator
+{
+	char* allocate(size_t size)
+	{
+		return mallocator::allocate(size+size_added_tc);
+	}
+	void deallocate(void* p)
+	{
+		mallocator::deallocate(p);
+	}
+};
+struct except_address_nothing
+{
+	bool operator()(void* ptr) {return false;}
+};
+template <typename mallocator,typename except_address_tn=except_address_nothing>
+struct cppmalloc_except : mallocator
+{
+	void deallocate(void* p)
+	{
+		if (!except_address_tn()(p))
+			mallocator::deallocate(p);
+	}
+};
+template <typename mallocator,size_t size_preadded_tc,size_t size_added_tc>
+struct cppmalloc_both_more : mallocator
+{
+	char* allocate(size_t size)
+	{
+		char* p = mallocator::allocate(size_preadded_tc+size+size_added_tc);
+		if (p) return p+size_preadded_tc;
+		return 0;
+	}
+	void deallocate(void* p)
+	{
+		if (p) mallocator::deallocate((char*)p-size_preadded_tc);
+	}
+};
+
+template <typename allocator_tn,size_t size_preadded_tc,size_t size_added_tc,typename except_tn>
+struct malloc_both_and_except : allocator_tn /// both and except
+{
+	char* allocate(size_t size)
+	{
+		char* p = allocator_tn::allocate(size_preadded_tc+size+size_added_tc);
+		if (p) return p+size_preadded_tc;
+		return 0;
+	}
+	void deallocate(void* ptr)
+	{
+		if ((except_tn()()+size_preadded_tc)!=ptr)
+			allocator_tn::deallocate((char*)ptr-size_preadded_tc);
+	}
+};
+
+template <typename relative_tn>
+struct cppmalloc_ref
+{
+	char* allocate(size_t size)
+	{
+		return static_cast<relative_tn*>(this)->allocator().allocate(size);
+	}
+	void deallocate(void* p)
+	{
+		static_cast<relative_tn*>(this)->allocator().deallocate(p);
+	}
+};
+template <typename allocator_tn>
+struct malloc_pointer_default
+{
+	typedef allocator_tn allocator_type;
+	char* allocate(size_t size)
+	{
+		return _m_allocator->allocate(size);
+	}
+	void deallocate(void* p)
+	{
+		_m_allocator->deallocate(p);
+	}
+
+	allocator_type* _m_allocator;
+};
+
+
+___namespace2_end()
---
+++ src/fit/vector.h
@@ -0,0 +1,372 @@
+
+#include <assert.h>
+#include "../ox/nsab.h"
+#include "allocator_default.h"
+
+
+
+___namespace2_begin(ox,fit)
+
+
+
+
+struct vector_helper
+{
+	template <typename object_tn>
+	static void destruct_objects(object_tn* begin,size_t size)
+	{
+		for (size_t i=0;i<size;++i)
+			(begin++)->~object_tn();
+	}
+	template <typename values_tn>
+	static void destruct_values(values_tn* begin,size_t size)
+	{
+	}
+	template <typename object_tn>
+	static void construct_objects(object_tn* begin,size_t size)
+	{
+		for (size_t i=0;i<size;++i)
+			new (begin++) object_tn();
+	}
+
+	template <typename object_tn>
+	static size_t copy_objects(object_tn* out,size_t out_size,object_tn* begin,size_t size)
+	{
+		if (out_size<size) return 0;
+		for (size_t i=0;i<size;++i)
+			*out++ = *begin++;
+		return size;
+	}
+	template <typename object_tn>
+	static size_t move_objects(object_tn* out,size_t out_size,object_tn* begin,size_t size)
+	{
+		if (out_size<size) return 0;
+		for (size_t i=0;i<size;++i)
+		{
+			*out++ = *begin;
+			begin->~object_tn();
+			++begin;
+		}
+		return size;
+	}
+	template <typename object_tn>
+	static size_t move_objects_backward(object_tn* out,size_t out_size,object_tn* begin,size_t size)
+	{
+		if (out_size<size || size==0) return 0;
+		out += size-1;
+		begin += size-1;
+		for (size_t i=size;i>0;--i)
+		{
+			*out-- = *begin;
+			begin->~object_tn();
+			--begin;
+		}
+		return size;
+	}
+};
+
+template<typename vector_rooter_tn>
+struct vector_tt;
+
+/// vector=>{head,allcotor}
+/// head=>{size,capacity,data_begin}
+/// data_begin=>{value,value,,,,}
+
+struct string_on_size_changed
+{
+	template <typename rooter_tn>
+	void operator()(rooter_tn& rooter,int size)
+	{
+		assert (rooter.size()+size<=rooter.capacity());
+		rooter._m_head._m_size += size;
+		*(rooter.data_begin()+rooter.size())=0;
+	}
+};
+
+struct vector_on_size_changed
+{
+	template <typename rooter_tn>
+	void operator()(rooter_tn& rooter,int size)
+	{
+		assert (rooter.size()+size<=rooter.capacity());
+		rooter._m_head._m_size += size;
+	}
+};
+template<typename value_tn,typename size_changed_tn=vector_on_size_changed,typename allocator_tn=cppmalloc>
+struct vector_rooter : allocator_tn
+{
+	friend vector_tt<vector_rooter>;
+	friend vector_on_size_changed;
+	friend string_on_size_changed;
+	typedef value_tn value_type;
+	typedef size_changed_tn on_size_changed;
+protected:
+	typedef vector_rooter self;
+	typedef allocator_tn allocator;
+	struct head_t
+	{
+		head_t() : _m_size(0), _m_capacity(0), _m_data_begin(0) {}
+		size_t _m_size;
+		size_t _m_capacity;
+		value_type* _m_data_begin;
+	};
+	~vector_rooter()
+	{
+		resize(0);
+		allocator::deallocate(_m_head._m_data_begin);
+		_m_head._m_data_begin = 0;
+	}
+	value_type* data_begin()
+	{
+		return _m_head._m_data_begin;
+	}
+	void exchange(self& other)
+	{
+		head_t t = _m_head;
+		_m_head = other._m_head;
+		other._m_head = t;
+	}
+	head_t _m_head;
+public:
+	size_t size() const
+	{
+		return _m_head._m_size;
+	}
+	size_t capacity() const
+	{
+		return _m_head._m_capacity;
+	}
+	void reserve(size_t size,size_t size_copyed=-1)
+	{
+		if (size_copyed==-1) size_copyed = _m_head._m_size;
+		size_t usable_size = _m_head._m_capacity-_m_head._m_size;
+		if (usable_size>=size) return;
+		value_type* data_begin = _m_head._m_data_begin;
+		data_begin = (value_type*)allocator::allocate((_m_head._m_size+size)*sizeof(value_type));
+		assert (data_begin);
+		vector_helper::move_objects(data_begin,-1,_m_head._m_data_begin,size_copyed);
+		_m_head._m_capacity = size;
+		_m_head._m_data_begin = data_begin;
+	}
+	void resize(size_t size)
+	{
+		if (size<=_m_head._m_size)
+		{
+			vector_helper::destruct_objects(_m_head._m_data_begin+size,_m_head._m_size-size);
+			_m_head._m_size = size;
+		}
+		else if (size<_m_head._m_capacity)
+		{
+			vector_helper::construct_objects(_m_head._m_data_begin+_m_head._m_size,size-_m_head._m_size);
+			_m_head._m_size = size;
+		}
+		else
+		{
+			reserve(size-_m_head._m_size);
+			vector_helper::construct_objects(_m_head._m_data_begin+_m_head._m_size,size-_m_head._m_size);
+			_m_head._m_size = size;
+		}
+		on_size_changed()(*this,0);
+	}
+};
+
+template<typename value_tn,typename size_changed_tn=vector_on_size_changed,typename allocator_tn=cppmalloc>
+struct string_rooter : vector_rooter<value_tn,size_changed_tn,allocator_tn>
+{
+	friend vector_tt<string_rooter>;
+	string_rooter()
+	{
+		_m_head._m_data_begin = allocator::allocate(0);
+		size_changed_tn()(*this,0);
+	}
+};
+
+
+template<typename array,size_t added,typename size_changed_tn=vector_on_size_changed>
+struct static_vector_rooter;
+template<typename value_tn,size_t n,size_t added,typename size_changed_tn>
+struct static_vector_rooter<value_tn[n],added,size_changed_tn>
+{
+	friend vector_tt<static_vector_rooter>;
+	friend vector_on_size_changed;
+	friend string_on_size_changed;
+	typedef value_tn value_type;
+	typedef value_tn array_type[n+added];
+protected:
+	typedef static_vector_rooter self;
+	typedef size_changed_tn on_size_changed;
+	struct head_t
+	{
+		head_t(): _m_size(0) {}
+		size_t _m_size;
+		array_type _m_data;
+	};
+	~static_vector_rooter()
+	{
+		_m_head._m_size = 0;
+	}
+	value_type* data_begin()
+	{
+		return _m_head._m_data;
+	}
+	void exchange(self& other)
+	{
+		assert (false && "not support");
+	}
+	head_t _m_head;
+public:
+	size_t size() const
+	{
+		return _m_head._m_size;
+	}
+	size_t capacity() const
+	{
+		return n;
+	}
+	void reserve(size_t size,size_t size_copyed=-1)
+	{
+	}
+	void resize(size_t size)
+	{
+		if (size<=_m_head._m_size)
+		{
+			vector_helper::destruct_objects(_m_head._m_data+size,_m_head._m_size-size);
+			_m_head._m_size = size;
+		}
+		else if (size<capacity())
+		{
+			vector_helper::construct_objects(_m_head._m_data+_m_head._m_size,size-_m_head._m_size);
+			_m_head._m_size = size;
+		}
+		else
+		{
+			reserve(size-_m_head._m_size);
+			vector_helper::construct_objects(_m_head._m_data+_m_head._m_size,size-_m_head._m_size);
+			_m_head._m_size = size;
+		}
+		on_size_changed()(*this,0);
+	}
+};
+
+template<typename array,typename size_changed_tn=string_on_size_changed>
+struct static_string_rooter;
+template<typename value_tn,size_t n,typename size_changed_tn>
+struct static_string_rooter<value_tn[n],size_changed_tn>
+	: static_vector_rooter<value_tn[n],1,size_changed_tn>
+{
+	friend vector_tt<static_string_rooter>;
+	static_vector_rooter<value_tn[n],1,size_changed_tn>::_m_head;
+	//*(static_vector_rooter<value_tn[n],1,size_changed_tn>::data_begin())=0;
+};
+
+template<typename vector_rooter_tn>
+struct vector_tt : vector_rooter_tn
+{
+	typedef vector_rooter_tn rooter;
+	typedef typename rooter::value_type value_type;
+
+	bool is_empty() const
+	{
+		return rooter::size()==0;
+	}
+	value_type* insert(size_t off,value_type const& value)
+	{
+		value_type* value_begin = 0;
+		if (rooter::size()>=rooter::capacity())
+		{
+			rooter _old_rooter;
+			rooter::exchange(_old_rooter);
+			rooter::reserve(_old_rooter.size()==0?2:_old_rooter.size()*2);
+			vector_helper::move_objects(rooter::data_begin(),-1,_old_rooter.data_begin(),off);
+			vector_helper::move_objects(rooter::data_begin()+off+1,-1,_old_rooter.data_begin()+off,_old_rooter.size()-off);
+			value_begin = rooter::data_begin()+off;
+			*value_begin = value;
+			rooter::on_size_changed()(*this,1+_old_rooter.size());
+		}
+		else
+		{
+			vector_helper::move_objects_backward(rooter::data_begin()+off+1,-1,rooter::data_begin()+off,rooter::size()-off);
+			value_begin = rooter::data_begin()+off;
+			*value_begin = value;
+			rooter::on_size_changed()(*this,1);
+		}
+		return value_begin;
+	}
+	value_type* push_front(value_type const& value)
+	{
+		return insert(0,value);
+	}
+	value_type* push_back(value_type const& value)
+	{
+		if (rooter::size()>=rooter::capacity())
+			rooter::reserve(is_empty()?2:rooter::size()*2);
+		assert(rooter::size()<rooter::capacity());
+		value_type* begin = rooter::data_begin();
+		value_type* added_begin = begin+rooter::size();
+		*added_begin = value;
+		rooter::on_size_changed()(root(),1);
+		return added_begin;
+	}
+	void erase(size_t off,size_t size)
+	{
+		assert(off<rooter::size() && off+size<rooter::size());
+		value_type* value_begin = rooter::data_begin();
+		value_type* out = value_begin+off;
+		value_type* src = out+size;
+		vector_helper::move_objects<value_type>(out,-1,src,rooter::size()-off-size);
+		rooter::on_size_changed(-size);
+	}
+	void erase(size_t i)
+	{
+		erase(off,1);
+	}
+	void erase_to_end(size_t i)
+	{
+		erase(i,rooter::size()-i);
+	}
+	value_type& at(size_t i)
+	{
+		assert (index<=rooter::size());
+		return *(rooter::data_begin()+i);
+	}
+	value_type const& at(size_t i) const
+	{
+		assert (index<=rooter::size());
+		return *(rooter::data_begin()+i);
+	}
+
+	template <typename action>
+	void foreach(action& act)
+	{
+		value_type* value_begin = rooter::data_begin();
+		for (size_t i=0;i<rooter::size();++i)
+		{
+			if (!act(*value_begin++)) break;
+		}
+	}
+	void clear()
+	{
+		rooter::resize(0);
+	}
+protected:
+	rooter& root() {return *this;}
+};
+
+template <typename value_tn,typename allocator_tn=cppmalloc>
+struct vector : vector_tt<vector_rooter<value_tn,vector_on_size_changed,allocator_tn>>
+{};
+
+template <typename array_tn>
+struct static_vector: vector_tt<static_vector_rooter<array_tn,0,vector_on_size_changed>>
+{};
+
+template <typename value_tn,typename allocator_tn=cppmalloc_more<cppmalloc,sizeof(value_tn)>>
+struct string : vector_tt<string_rooter<value_tn,string_on_size_changed,allocator_tn>>
+{};
+
+template <typename array_tn>
+struct static_string: vector_tt<static_string_rooter<array_tn,string_on_size_changed>>
+{};
+
+
+___namespace2_end()
